6a713a6a0d020938aae32d1ab360127997ea6a3e
AdityaJain-007 <adityajain.9339@gmail.com>
2026-09-14 23:50:10 +0530

    Add selection sort solution

---
+++ sorting/selection_sort/solution.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void selection_sort(int arr[], int size_of_array);
+int main()
+{
+    int size;
+    printf("enter the size of the array \n");
+    scanf("%d", &size);
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        printf("enter the value in the index %d:\n", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("array is printed before shorted :");
+    for(int i =0 ; i<size;i++){
+        printf("%d \n ", arr[i]);
+    }
+    selection_sort(arr, size);
+    printf("array is printed after shorted :");
+    for(int i =0 ; i<size;i++){
+        printf("%d \n ", arr[i]);
+    }
+}
+void selection_sort(int arr[], int size_of_array){
+    for(int i = 0; i<= size_of_array-2;i++){
+        int min =i, j ;
+        for( j =i ; j<= size_of_array-1 ;j++){
+            if(arr[j]<arr[min]){
+                min =j ;
+            }
+        }
+    int temp = arr[i];
+    arr[i]= arr[j];
+    arr[j]= temp;
+    }
+}
