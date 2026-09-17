34a3568e31f0a4337bddb3db80e1aa01ddebc5d0
AdityaJain-007 <adityajain.9339@gmail.com>
2026-09-17 23:47:02 +0530

    Done Second largest element !!

--- arrays/basic/array_Second_Largest_element/solution.c
+++ arrays/basic/array_Second_Largest_element/solution.c
@@ -1,47 +1,76 @@
-#include<stdio.h>
-int second_largest(int arr[], int size){
-    if (size< 2) {
+#include <stdio.h>
+    
+#include <limits.h> // this the unique provide the max or min of any data type   like ---int first = INT_MIN;
+
+int second_largest(int arr[], int size)
+{
+    if (size < 2)
+    {
         printf("Array must have at least two elements.\n");
         return -1;
     }
-    int first , second ;
-    first= second=arr[1];
-    for(int i =2 ; i<size;i++){
-        if(arr[i]>first){
-            second= first;
-            first= arr[i];
+
+    int first = INT_MIN;
+    int second = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > first)
+        {
+            second = first;
+            first = arr[i];
         }
-        else if (arr[i] > second && arr[i] != first) {
+        else if (arr[i] > second && arr[i] != first)
+        {
             second = arr[i];
         }
     }
+
+    if (second == INT_MIN)
+    {
+        printf("There is no second largest distinct element.\n");
+        return -1;
+    }
+
     return second;
 }
+
 int main()
 {
     int size;
-    printf("enter the size of the array \n");
+
+    printf("Enter the size of the array:\n");
     scanf("%d", &size);
+
     if (size <= 0)
     {
         printf("Invalid array size.\n");
         return 1;
     }
+
     int arr[size];
 
     for (int i = 0; i < size; i++)
     {
-        printf("enter the value in the index %d:\n", i + 1);
+        printf("Enter the value at index %d:\n", i);
         scanf("%d", &arr[i]);
     }
-    printf("array you inserted is : \n ");
+
+    printf("Array you inserted is:\n");
+
     for (int i = 0; i < size; i++)
     {
-        printf("%d \n ", arr[i]);
+        printf("%d\n", arr[i]);
     }
-    int a = second_largest(arr,size);
-    
-    
+
     
 
+    int result = second_largest(arr, size);
+
+    if (result != -1)
+    {
+        printf("The second largest distinct element is: %d\n", result);
+    }
+
+    return 0;
 }
